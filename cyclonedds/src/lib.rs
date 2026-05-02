65f35167dee166a4281389936a402389a93abe0c
mzet97 <matheus.zeitune.developer@gmail.com>
2026-05-02 14:09:36 -0300

    fix: usa feature std em vez de not(no_std) para compatibilidade com --all-features

--- cyclonedds/src/lib.rs
+++ cyclonedds/src/lib.rs
@@ -1,156 +1,152 @@
-#![cfg_attr(feature = "no_std", no_std)]
+#![cfg_attr(all(feature = "no_std", not(feature = "std")), no_std)]
 #![allow(missing_docs)]
 
 extern crate alloc;
 
 /// Safe, idiomatic Rust bindings for Eclipse CycloneDDS.
-#[cfg(not(feature = "no_std"))]
 pub mod no_std_types;
 
-#[cfg(feature = "no_std")]
-pub mod no_std_types;
-
-#[cfg(feature = "no_std")]
+#[cfg(not(feature = "std"))]
 pub use no_std_types::*;
 
-#[cfg(not(feature = "no_std"))]
+#[cfg(feature = "std")]
 pub use cyclonedds_derive::DdsBitmask as DdsBitmaskDerive;
-#[cfg(not(feature = "no_std"))]
+#[cfg(feature = "std")]
 pub use cyclonedds_derive::DdsEnum as DdsEnumDerive;
-#[cfg(not(feature = "no_std"))]
+#[cfg(feature = "std")]
 pub use cyclonedds_derive::DdsType as DdsTypeDerive;
-#[cfg(not(feature = "no_std"))]
+#[cfg(feature = "std")]
 pub use cyclonedds_derive::DdsUnion as DdsUnionDerive;
 
-#[cfg(all(feature = "async", not(feature = "no_std")))]
+#[cfg(all(feature = "async", feature = "std"))]
 #[allow(missing_docs)]
 pub mod r#async;
-#[cfg(not(feature = "no_std"))]
+#[cfg(feature = "std")]
 #[allow(missing_docs)]
 mod builtin;
-#[cfg(not(feature = "no_std"))]
+#[cfg(feature = "std")]
 #[allow(missing_docs)]
 mod content_filtered_topic;
-#[cfg(not(feature = "no_std"))]
+#[cfg(feature = "std")]
 #[allow(missing_docs)]
 mod dynamic_type;
-#[cfg(not(feature = "no_std"))]
+#[cfg(feature = "std")]
 #[allow(missing_docs)]
 mod dynamic_value;
-#[cfg(not(feature = "no_std"))]
+#[cfg(feature = "std")]
 #[allow(missing_docs)]
 mod entity;
 #[allow(missing_docs)]
 mod error;
-#[cfg(not(feature = "no_std"))]
+#[cfg(feature = "std")]
 #[allow(missing_docs)]
 mod listener;
-#[cfg(all(any(feature = "opentelemetry", feature = "tokio-console"), not(feature = "no_std")))]
+#[cfg(all(any(feature = "opentelemetry", feature = "tokio-console"), feature = "std"))]
 pub mod observability;
-#[cfg(not(feature = "no_std"))]
+#[cfg(feature = "std")]
 pub mod log;
-#[cfg(not(feature = "no_std"))]
+#[cfg(feature = "std")]
 mod participant;
-#[cfg(not(feature = "no_std"))]
+#[cfg(feature = "std")]
 mod participant_pool;
-#[cfg(not(feature = "no_std"))]
+#[cfg(feature = "std")]
 mod publisher;
-#[cfg(not(feature = "no_std"))]
+#[cfg(feature = "std")]
 mod qos;
-#[cfg(not(feature = "no_std"))]
+#[cfg(feature = "std")]
 mod request_reply;
-#[cfg(not(feature = "no_std"))]
+#[cfg(feature = "std")]
 mod qos_provider;
-#[cfg(not(feature = "no_std"))]
+#[cfg(feature = "std")]
 mod reader;
-#[cfg(not(feature = "no_std"))]
+#[cfg(feature = "std")]
 pub mod sample;
-#[cfg(all(feature = "serde", not(feature = "no_std")))]
+#[cfg(all(feature = "serde", feature = "std"))]
 mod serde_sample;
-#[cfg(all(feature = "security", not(feature = "no_std")))]
+#[cfg(all(feature = "security", feature = "std"))]
 pub mod security;
-#[cfg(not(feature = "no_std"))]
+#[cfg(feature = "std")]
 mod sequence;
-#[cfg(not(feature = "no_std"))]
+#[cfg(feature = "std")]
 mod serialization;
-#[cfg(not(feature = "no_std"))]
+#[cfg(feature = "std")]
 mod statistics;
-#[cfg(not(feature = "no_std"))]
+#[cfg(feature = "std")]
 mod status;
-#[cfg(not(feature = "no_std"))]
+#[cfg(feature = "std")]
 mod string;
-#[cfg(not(feature = "no_std"))]
+#[cfg(feature = "std")]
 mod subscriber;
-#[cfg(not(feature = "no_std"))]
+#[cfg(feature = "std")]
 mod topic;
-#[cfg(not(feature = "no_std"))]
+#[cfg(feature = "std")]
 mod type_discovery;
-#[cfg(not(feature = "no_std"))]
+#[cfg(feature = "std")]
 mod waitset;
-#[cfg(not(feature = "no_std"))]
+#[cfg(feature = "std")]
 #[doc(hidden)]
 pub mod write_arena;
-#[cfg(not(feature = "no_std"))]
+#[cfg(feature = "std")]
 mod writer;
-#[cfg(not(feature = "no_std"))]
+#[cfg(feature = "std")]
 mod xtypes;
 
-#[cfg(not(feature = "no_std"))]
+#[cfg(feature = "std")]
 pub use builtin::{
     BuiltinEndpointSample, BuiltinParticipantSample, BuiltinTopicSample,
     BUILTIN_TOPIC_DCPSPARTICIPANT, BUILTIN_TOPIC_DCPSPUBLICATION, BUILTIN_TOPIC_DCPSSUBSCRIPTION,
     BUILTIN_TOPIC_DCPSTOPIC, DDS_MIN_PSEUDO_HANDLE,
 };
-#[cfg(not(feature = "no_std"))]
+#[cfg(feature = "std")]
 pub use content_filtered_topic::{ContentFilteredTopic, FilterParams, TopicFilterExt, TopicParameterizedFilterExt};
-#[cfg(not(feature = "no_std"))]
+#[cfg(feature = "std")]
 pub use dynamic_type::{
     DynamicEnumLiteralValue, DynamicMemberBuilder, DynamicPrimitiveKind, DynamicType,
     DynamicTypeAutoId, DynamicTypeBuilder, DynamicTypeExtensibility, DynamicTypeSpec,
 };
-#[cfg(not(feature = "no_std"))]
+#[cfg(feature = "std")]
 pub use dynamic_value::{
     DynamicBitmaskFieldSchema, DynamicData, DynamicEnumLiteralSchema, DynamicFieldSchema,
     DynamicTypeSchema, DynamicUnionCaseSchema, DynamicValue,
 };
-#[cfg(not(feature = "no_std"))]
+#[cfg(feature = "std")]
 pub use entity::DdsEntity;
-#[cfg(not(feature = "no_std"))]
+#[cfg(feature = "std")]
 pub use error::{err_file_id, err_line, err_nr};
 pub use error::{DdsError, DdsResult};
-#[cfg(not(feature = "no_std"))]
+#[cfg(feature = "std")]
 pub use listener::{Listener, ListenerBuilder};
-#[cfg(not(feature = "no_std"))]
+#[cfg(feature = "std")]
 pub use participant::DomainParticipant;
-#[cfg(not(feature = "no_std"))]
+#[cfg(feature = "std")]
 pub use publisher::Publisher;
-#[cfg(not(feature = "no_std"))]
+#[cfg(feature = "std")]
 pub use qos::{
     DataRepresentation, DestinationOrder, Durability, DurabilityServicePolicy, History,
     IgnoreLocalKind, Liveliness, Ownership, PresentationAccessScope, PresentationPolicy, Qos,
     QosBuilder, ReaderDataLifecyclePolicy, Reliability, TypeConsistency, TypeConsistencyPolicy,
 };
-#[cfg(not(feature = "no_std"))]
+#[cfg(feature = "std")]
 pub use participant_pool::{DiscoveredParticipant, DiscoveredTopic, ParticipantPool};
-#[cfg(not(feature = "no_std"))]
+#[cfg(feature = "std")]
 pub use qos_provider::{QosKind, QosProvider};
-#[cfg(not(feature = "no_std"))]
+#[cfg(feature = "std")]
 pub use request_reply::{Replier, RequestReply, Requester};
-#[cfg(not(feature = "no_std"))]
+#[cfg(feature = "std")]
 pub use reader::DataReader;
-#[cfg(not(feature = "no_std"))]
+#[cfg(feature = "std")]
 pub use sample::{Loan, Sample};
-#[cfg(all(feature = "serde", not(feature = "no_std")))]
+#[cfg(all(feature = "serde", feature = "std"))]
 pub use serde_sample::SerdeSample;
-#[cfg(all(feature = "security", not(feature = "no_std")))]
+#[cfg(all(feature = "security", feature = "std"))]
 pub use security::SecurityConfig;
-#[cfg(not(feature = "no_std"))]
+#[cfg(feature = "std")]
 pub use sequence::{DdsBoundedSequence, DdsSequence, DdsSequenceElement};
-#[cfg(not(feature = "no_std"))]
+#[cfg(feature = "std")]
 pub use serialization::{CdrDeserializer, CdrEncoding, CdrSample, CdrSerializer};
-#[cfg(not(feature = "no_std"))]
+#[cfg(feature = "std")]
 pub use statistics::{StatisticEntryRef, StatisticKind, StatisticValue, Statistics};
-#[cfg(not(feature = "no_std"))]
+#[cfg(feature = "std")]
 pub use status::{
     EntityStatus, InconsistentTopicStatus, LivelinessChangedStatus, LivelinessLostStatus,
     OfferedDeadlineMissedStatus, OfferedIncompatibleQosStatus, PublicationMatchedStatus,
@@ -162,11 +158,11 @@ pub use status::{
     STATUS_REQUESTED_INCOMPATIBLE_QOS, STATUS_SAMPLE_LOST, STATUS_SAMPLE_REJECTED,
     STATUS_SUBSCRIPTION_MATCHED,
 };
-#[cfg(not(feature = "no_std"))]
+#[cfg(feature = "std")]
 pub use string::DdsString;
-#[cfg(not(feature = "no_std"))]
+#[cfg(feature = "std")]
 pub use subscriber::Subscriber;
-#[cfg(not(feature = "no_std"))]
+#[cfg(feature = "std")]
 pub use topic::{
     adr, adr_bst, adr_key, rebase_ops, DdsEnumType, DdsType, DdsUnionType, DiscriminantType,
     KeyDescriptor, Topic, TopicKeyDescriptor, UntypedTopic, DDS_OP_MASK_CONST,
@@ -176,17 +172,17 @@ pub use topic::{
     SUBTYPE_ENU, SUBTYPE_SEQ, SUBTYPE_STU, SUBTYPE_STR, TYPE_1BY, TYPE_2BY, TYPE_4BY, TYPE_8BY,
     TYPE_ARR, TYPE_BSQ, TYPE_BST, TYPE_ENU, TYPE_EXT, TYPE_SEQ, TYPE_STR, TYPE_UNI,
 };
-#[cfg(not(feature = "no_std"))]
+#[cfg(feature = "std")]
 pub use type_discovery::{
     cdr_to_dynamic_data, discover_all_publication_types, discover_all_subscription_types,
     discover_type_from_endpoint, discover_type_from_publication, discover_type_from_subscription,
     discover_type_from_type_info, dynamic_data_to_cdr, DiscoveredType,
 };
-#[cfg(not(feature = "no_std"))]
+#[cfg(feature = "std")]
 pub use waitset::{set_active_qc, GuardCondition, QueryCondition, ReadCondition, WaitSet};
-#[cfg(not(feature = "no_std"))]
+#[cfg(feature = "std")]
 pub use writer::{DataWriter, WriteLoan};
-#[cfg(not(feature = "no_std"))]
+#[cfg(feature = "std")]
 pub use xtypes::{
     FindScope, MatchedEndpoint, MemberDescriptor, OwnedSertype, OwnedTypeId, SertypeHandle,
     TopicDescriptor, TypeDescriptor, TypeExtensibility, TypeIdKind, TypeIdRef, TypeIncludeDeps,
